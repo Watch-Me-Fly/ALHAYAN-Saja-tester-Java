3ce02f62322b0bc8daf87134cbab119b84468a72
watch-me-fly <thi.phoned@gmail.com>
2024-12-01 14:43:49 +0100

    New test : 5% discount for regular cars

--- src/test/java/com/parkit/parkingsystem/calculateFareCarWithDiscount.java
+++ src/test/java/com/parkit/parkingsystem/calculateFareCarWithDiscount.java
@@ -1,4 +1,50 @@
 package com.parkit.parkingsystem;
 
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+
+// ce test doit appeler la méthode calculateFare avec un ticket concernant une voiture
+// et avec le paramètre discount à true,
+// puis vérifier que le prix calculé est est
+// bien de 95% du tarif plein. La durée du ticket doit être de plus de 30 minutes.
+
 public class calculateFareCarWithDiscount {
+    private static FareCalculatorService fareCalculatorService;
+    private Ticket ticket;
+
+    @BeforeAll
+    public static void setUp() {
+        fareCalculatorService = new FareCalculatorService();
+    }
+
+    @BeforeEach
+    public void setUpTicket() {
+        ticket = new Ticket();
+    }
+
+    @Test
+    public void carWithDiscount() {
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+        ticket.setParkingSpot(parkingSpot);
+
+        Date inTime = new Date();
+        inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000)); // 1h parking
+        Date outTime = new Date();
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+
+        fareCalculatorService.calculateFare(ticket, true);
+
+        double expectedPrice = Fare.CAR_RATE_PER_HOUR * 0.95; // 5% discount
+        assertEquals(expectedPrice,ticket.getPrice(),0, "Should give a 5% discount for regulars in cars.");
+    }
 }
